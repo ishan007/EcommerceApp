8b470c2d842babec39d3f2a53fdcc5d786b87ef0
Ishan Gaurav <Ishangaurav@gmail.com>
2023-04-03 14:54:30 +0530

    Updated jenkins file

--- app/src/main/java/com/example/nagpecommerce/retrofit/ApiClient.kt
+++ app/src/main/java/com/example/nagpecommerce/retrofit/ApiClient.kt
@@ -3,8 +3,8 @@ package com.example.nagpecommerce.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val USER_SERVICE_URL = "http://20.237.26.95:8081"
-const val SEARCH_SERVICE_URL = "http://20.242.131.172:8082/search/products"
+const val USER_SERVICE_URL = "http://20.124.157.178:8081"
+const val SEARCH_SERVICE_URL = "http://20.124.157.178:8082/search/products"
 const val LOGIN_PATH = "/user/login"
 const val REGISTER_PATH = "/user/register"
 object ApiClient {
