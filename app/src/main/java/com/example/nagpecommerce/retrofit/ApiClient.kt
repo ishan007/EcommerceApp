49d37c68d0a4f34663ec3f5b6d7d46f8c864fbcb
Ishan Gaurav <Ishangaurav@gmail.com>
2023-04-05 18:06:33 +0530

    Added host

--- app/src/main/java/com/example/nagpecommerce/retrofit/ApiClient.kt
+++ app/src/main/java/com/example/nagpecommerce/retrofit/ApiClient.kt
@@ -3,8 +3,8 @@ package com.example.nagpecommerce.retrofit
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-const val USER_SERVICE_URL = "http://20.124.157.178:8081"
-const val SEARCH_SERVICE_URL = "http://20.124.157.178:8082/search/products"
+const val USER_SERVICE_URL = "http://40.76.169.194:8081"
+const val SEARCH_SERVICE_URL = "http://40.76.169.194:8082/search/products"
 const val LOGIN_PATH = "/user/login"
 const val REGISTER_PATH = "/user/register"
 object ApiClient {
